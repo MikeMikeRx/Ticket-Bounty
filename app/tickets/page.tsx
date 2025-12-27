94bdd7fe7f7dd9d4da365f992a9726630597ccb3
MichalRais <Michal.Rais@iCloud.com>
2025-12-27 18:54:22 +0000

    feat(routing): centralize route paths in constants and refactor page links

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { initialTickets } from "@/data/data";
+import { ticketPath } from "@/constants/paths";
 
 const TicketPage = () => {
     return (
@@ -8,11 +9,11 @@ const TicketPage = () => {
                 <div key={ticket.id}>
                     <h2 className="text-lg">{ticket.title}</h2>
 
-                    <Link href={`/tickets/${ticket.id}`} className="text-sm underline">
+                    <Link href={ticketPath(ticket.id)} className="text-sm underline">
                         View
                     </Link> 
                 </div>
-            ))}        
+            ))}
         </div>
     );
 };
