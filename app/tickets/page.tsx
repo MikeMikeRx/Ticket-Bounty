526c1bc75a1751001f429b813a50893b607a6295
Michal Rais <Michal.Rais@icloud.com>
2026-01-13 09:15:53 +0100

    feat(ticket): add ticket creation form with server action

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,3 +1,5 @@
+import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
+import { TicketCreateForm } from "@/features/ticket/components/ticket-create-form";
 import { Suspense } from "react";
 import { Heading } from "@/components/heading";
 import { Spinner } from "@/components/spinner";
@@ -9,6 +11,16 @@ const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="Tickets" description="All your tickets at one place" />
+
+            <Card className="w-full max-w-105 self-center">
+                <CardHeader>
+                    <CardTitle>Create Ticket</CardTitle>
+                    <CardDescription>A new ticket will be created</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <TicketCreateForm />
+                </CardContent>
+            </Card>
             
             <Suspense fallback={<Spinner/>}>
                 <TicketList />
