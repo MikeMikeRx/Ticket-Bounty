096e3d5839a99758015e38243d5216274678aa3e
Michal Rais <Michal.Rais@icloud.com>
2026-01-15 19:58:29 +0100

    refactor(form): encapsulate action state handling in Form component

--- components/form/form.tsx
+++ components/form/form.tsx
@@ -1,14 +1,19 @@
+import { useActionState } from "react";
 import { toast } from "sonner";
 import { useActionFeedback } from "./hooks/use-action-feedback";
-import { ActionState } from "./utils/to-action-state";
+import { EMPTY_ACTION_STATE, ActionState } from "./utils/to-action-state";
 
 type FormProps = {
-    action: (payload: FormData) => void;
-    actionState: ActionState;
-    children: React.ReactNode;
+    action: (
+        actionState: ActionState,
+        formData: FormData
+    ) => ActionState | Promise <ActionState>;
+    children: (actionState: ActionState) => React.ReactNode;
 };
 
-const Form = ({ action, actionState, children }: FormProps) => {
+const Form = ({ action, children }: FormProps) => {
+    const [actionState, formAction] = useActionState(action, EMPTY_ACTION_STATE);
+
     useActionFeedback(actionState, {
         onSuccess: ({ actionState }) => {
             if (actionState.message){
@@ -23,8 +28,18 @@ const Form = ({ action, actionState, children }: FormProps) => {
     });
 
     return (
-        <form action={action} className="flex flex-col gap-y-2">
-            {children}
+        <form action={formAction} className="flex flex-col gap-y-2">
+            {children(actionState)}
+
+            <noscript>
+                {actionState.status === "ERROR" && (
+                    <div style={{ color: "red" }}>{actionState.message}</div>
+                )}
+
+                {actionState.status === "SUCCESS" && (
+                    <div style={{ color: "green" }}>{actionState.message}</div>
+                )}
+            </noscript>
         </form>
     );
 };
--- features/ticket/components/ticket-upsert-form.tsx
+++ features/ticket/components/ticket-upsert-form.tsx
@@ -1,13 +1,9 @@
 "use client";
 
 import { Ticket } from "@prisma/client";
-import { useActionState } from "react";
-import { toast } from "sonner";
 import { SubmitButton } from "@/components/form/submit-button";
 import { FieldError } from "@/components/form/field-error";
 import { Form } from "@/components/form/form";
-import { useActionFeedback } from "@/components/form/hooks/use-action-feedback";
-import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
@@ -18,35 +14,34 @@ type TicketUpsertFormProps = {
 };
 
 const TicketUpsertForm = ({ ticket }: TicketUpsertFormProps) => {
-    const [actionState, action] = useActionState(
-        upsertTicket.bind(null, ticket?.id),
-        EMPTY_ACTION_STATE
-    );
-
     return (
-        <Form action={action} actionState={actionState}>
-            <Label htmlFor="title">Title</Label>
-            <Input
-                id="title"
-                name="title"
-                type="text"
-                defaultValue={
-                  (actionState.payload?.get("title") as string) ?? ticket?.title
-                }
-            />
-            <FieldError actionState={actionState} name="title" />
+        <Form action={upsertTicket.bind(null, ticket?.id)}>
+            {(actionState) => (
+                <>
+                    <Label htmlFor="title">Title</Label>
+                    <Input
+                        id="title"
+                        name="title"
+                        type="text"
+                        defaultValue={
+                        (actionState.payload?.get("title") as string) ?? ticket?.title
+                        }
+                    />
+                    <FieldError actionState={actionState} name="title" />
 
-            <Label htmlFor="content">Content</Label>
-            <Textarea
-                id="content"
-                name="content"
-                defaultValue={
-                   (actionState.payload?.get("content") as string) ?? ticket?.content
-                }
-            />
-            <FieldError actionState={actionState} name="content" />
+                    <Label htmlFor="content">Content</Label>
+                    <Textarea
+                        id="content"
+                        name="content"
+                        defaultValue={
+                        (actionState.payload?.get("content") as string) ?? ticket?.content
+                        }
+                    />
+                    <FieldError actionState={actionState} name="content" />
 
-            <SubmitButton label={ticket ? "Edit" : "Create"} />
+                    <SubmitButton label={ticket ? "Edit" : "Create"} />                
+                </>
+            )}
         </Form>
     );
 };
