9d7bcea7c5548a264d51b7da2c1a089aa8c26652
Michal Rais <Michal.Rais@icloud.com>
2026-01-29 19:11:34 +0000

    feat(ticket): add toast feedback for delete action

--- features/ticket/actions/delete-ticket.ts
+++ features/ticket/actions/delete-ticket.ts
@@ -5,13 +5,20 @@ import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/constants/paths";
 import { setCookieByKey } from "@/actions/cookies";
+import { fromErrorToActionState } from "@/components/form/utils/to-action-state";
 
 export const deleteTicket = async (id:string) => {
-    await prisma.ticket.delete({
-        where: {
-            id,
-        },
-    });
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    try {
+        await prisma.ticket.delete({
+            where: {
+                id,
+            },
+        });
+    } catch (error) {
+        return fromErrorToActionState(error);
+    }
 
     revalidatePath(ticketsPath());
     await setCookieByKey("toast", "Ticket deleted");
