c44dd5c22d1e4a4385aeda5863e780c9cc56f753
Michal Rais <Michal.Rais@icloud.com>
2026-02-09 09:32:39 +0000

    feat: add search input synced with URL query params

--- features/ticket/components/ticket-list.tsx
+++ features/ticket/components/ticket-list.tsx
@@ -1,5 +1,6 @@
 import { TicketItem } from "./ticket-item";
 import { getTickets } from "../queries/get-tickets";
+import { SearchInput } from "@/components/search-input";
 
 type TicketListProps = {
     userId?: string;
@@ -10,6 +11,10 @@ const TicketList = async ({ userId }: TicketListProps) => {
 
     return (
         <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
+            <div className="w-full max-w-110">
+                <SearchInput placeholder="Search tickets" />
+            </div>
+
             {tickets.map((ticket) => (
                 <TicketItem key={ticket.id} ticket={ticket}/>
             ))}
