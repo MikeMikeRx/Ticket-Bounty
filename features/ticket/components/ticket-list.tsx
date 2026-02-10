f5093e73add29b833fa7127eb5a576587ecf96fa
Michal Rais <Michal.Rais@icloud.com>
2026-02-10 07:59:25 +0000

    feat(sort): add Oldest option using createdAt asceding

--- features/ticket/components/ticket-list.tsx
+++ features/ticket/components/ticket-list.tsx
@@ -26,6 +26,11 @@ const TicketList = async ({ userId, searchParams }: TicketListProps) => {
                             label: "Newest",
                         },
                         {
+                            sortKey: "createdAt",
+                            sortValue: "asc",
+                            label: "Oldest",
+                        },
+                        {
                             sortKey: "bounty",
                             sortValue: "desc",
                             label: "Bounty",
