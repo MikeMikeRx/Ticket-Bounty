cd7300f810f84bdda83a9ace6552869b464edbdc
Michal Rais <Michal.Rais@icloud.com>
2026-02-11 15:19:25 +0000

    feat(comments): implement comment delete action with confirmation dialog

--- features/comment/components/comments.tsx
+++ features/comment/components/comments.tsx
@@ -2,6 +2,9 @@ import { CardCompact } from "@/components/card-compact";
 import { getComments } from "../queries/get-comments";
 import { CommentCreateForm } from "./comment-create-form";
 import { CommentItem } from "./comment-item";
+import { CommentDeleteButton } from "./comment-delete-button";
+import { getAuth } from "@/features/auth/queries/get-auth";
+import { isOwner } from "@/features/auth/utils/is-owner";
 
 type CommentsProps = {
     ticketId: string;
@@ -9,6 +12,7 @@ type CommentsProps = {
 
 const Comments = async ({ ticketId }: CommentsProps) => {
     const comments = await getComments(ticketId);
+    const { user } = await getAuth();
 
     return (
         <>
@@ -20,7 +24,17 @@ const Comments = async ({ ticketId }: CommentsProps) => {
 
             <div className="flx flex-col gap-y-2 ml-8">
                 {comments.map((comment) => (
-                    <CommentItem key={comment.id} comment={comment} />
+                    <CommentItem
+                        key={comment.id}
+                        comment={comment}
+                        buttons={[
+                            ...(isOwner(user, comment)
+                            ? [
+                                <CommentDeleteButton key="0" id={comment.id} />
+                            ]
+                            : []),
+                        ]}
+                    />
                 ))}
             </div>
         </>
