5789d2ea006fe943d6e359c9005195b8773730a4
Michal Rais <Michal.Rais@icloud.com>
2026-02-19 09:52:18 +0000

    feat(comments): add client-side state update on comment create

--- features/comment/components/comments.tsx
+++ features/comment/components/comments.tsx
@@ -35,12 +35,22 @@ const Comments = ({ ticketId, paginatedComments }: CommentsProps) => {
         );
     };
 
+    const handleCreateComment = (comment: CommentWithMetaData | undefined) => {
+        if(!comment) return;
+
+        setComments((prevComments) => [comment, ...prevComments]);
+    };
+
     return (
         <>
             <CardCompact
                 title="Create Comment"
                 description="A new comment will be created"
-                content={<CommentCreateForm ticketId={ticketId} />}
+                content={
+                    <CommentCreateForm
+                        ticketId={ticketId}
+                        onCreateComment={handleCreateComment}
+                    />}
             />
 
             <div className="flex flex-col gap-y-2 ml-8">
